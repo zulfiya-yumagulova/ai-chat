a4ab9ed3200ecc4da0613c3b939011ce199db7a4
Zulfiya Yumagulova <yumagulovazu@gmail.com>
2024-03-20 22:41:28 +0000

    basic set up routes

--- client/ai-friends/src/App.jsx
+++ client/ai-friends/src/App.jsx
@@ -1,9 +1,25 @@
+import { Routes, Route } from "react-router-dom";
+import Header from "./components/Header";
 import "./App.css";
+import Home from "./pages/Home";
+import Login from "./pages/Login";
+import Signup from "./pages/Signup";
+import Chat from "./pages/Chat";
+import NotFound from "./pages/NotFound";
 
 function App() {
   return (
     <>
-      <p>AI Friends</p>
+      <main>
+        <Header />
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/login" element={<Login />} />
+          <Route path="/signup" element={<Signup />} />
+          <Route path="/chat" element={<Chat />} />
+          <Route path="/notFound" element={<NotFound />} />
+        </Routes>
+      </main>
     </>
   );
 }
