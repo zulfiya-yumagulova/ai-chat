79078566fdc4088fc5bfd831f597484efc04c08d
Zulfiya Yumagulova <yumagulovazu@gmail.com>
2024-03-20 22:32:11 +0000

    front end basic set up packages

--- client/ai-friends/src/main.jsx
+++ client/ai-friends/src/main.jsx
@@ -1,10 +1,25 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
-import './index.css'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import { createTheme, ThemeProvider } from "@mui/material";
+import { BrowserRouter } from "react-router-dom";
+import App from "./App.jsx";
+import "./index.css";
 
-ReactDOM.createRoot(document.getElementById('root')).render(
+const theme = createTheme({
+  typography: {
+    fontFamily: "Roboto Slab, serif",
+    allVariants: {
+      color: "#31363f",
+    },
+  },
+});
+
+ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-)
+    <BrowserRouter>
+      <ThemeProvider theme={theme}>
+        <App />
+      </ThemeProvider>
+    </BrowserRouter>
+  </React.StrictMode>
+);
