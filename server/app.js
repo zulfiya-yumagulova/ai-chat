c767f9192c990cd7516cea2aebb5d7fa0d77eccb
Zulfiya Yumagulova <yumagulovazu@gmail.com>
2024-03-22 19:48:42 +0000

    connect to data base

--- server/app.js
+++ server/app.js
@@ -1,10 +1,18 @@
 import express from "express";
+import mongoose from "mongoose";
 import * as dotenv from "dotenv";
 dotenv.config();
 
 const app = express();
 const PORT = process.env.PORT;
+const DB = process.env.MONGODB_URL;
 
-app.listen(PORT, () => {
-  console.log(`App runs on port ${PORT}`);
+// Middleware
+app.use(express.json());
+
+mongoose.connect(DB).then(() => {
+  console.log("DB connection is successful");
+  app.listen(PORT, () => {
+    console.log(`App runs on port ${PORT}`);
+  });
 });
