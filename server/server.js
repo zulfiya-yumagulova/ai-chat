d8ea951d443987a7dc465c5b2b7f3c3c207f080d
Zulfiya Yumagulova <yumagulovazu@gmail.com>
2024-03-28 16:23:33 +0000

    fix: connected to DB

--- server/server.js
+++ server/server.js
@@ -1,13 +1,17 @@
 import * as dotenv from "dotenv";
 dotenv.config();
+import mongoose from "mongoose";
 import { app } from "./app.js";
-import { connectToDatabase } from "./db/connection.js";
-const PORT = process.env.PORT;
 
-connectToDatabase()
+const PORT = process.env.PORT;
+const DB = process.env.MONGODB_URL;
+
+mongoose
+  .connect(DB)
   .then(() => {
     app.listen(PORT, () => {
       console.log(`App runs on port ${PORT}`);
     });
+    console.log("Successfully connected to MongoDB");
   })
   .catch((error) => console.log(error));
