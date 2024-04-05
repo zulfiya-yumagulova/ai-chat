ae5b3a1bc5fff7adbcb43618ce518ca9de753a7c
Zulfiya Yumagulova <yumagulovazu@gmail.com>
2024-04-05 18:06:47 +0100

    add NavigationLink to Header component

--- client/ai-friends/src/components/Header.jsx
+++ client/ai-friends/src/components/Header.jsx
@@ -1,8 +1,12 @@
 import AppBar from "@mui/material/AppBar";
 import ToolBar from "@mui/material/ToolBar";
 import Logo from "../shared/Logo";
+import { useAuth } from "../context/AuthContext";
+import NavigationLink from "../shared/NavigationLink";
 
 function Header() {
+  const auth = useAuth();
+
   return (
     <AppBar
       style={{
@@ -13,6 +17,35 @@ function Header() {
     >
       <ToolBar style={{ display: "flex" }}>
         <Logo />
+        <div>
+          {auth?.isLoggedIn ? (
+            <>
+              <NavigationLink
+                bg="#000fffc"
+                to="/chat"
+                text="Go to chat"
+                textColor="#000"
+              />
+              <NavigationLink
+                bg="#51538f"
+                textColor="white"
+                to="/"
+                text="logout"
+                onClick={auth.logout}
+              />
+            </>
+          ) : (
+            <>
+              <NavigationLink
+                bg="#000fffc"
+                to="/login"
+                text="Go to chat"
+                textColor="#000"
+              />
+              <NavigationLink bg="#51538f" textColor="white" to="/signup" />
+            </>
+          )}
+        </div>
       </ToolBar>
     </AppBar>
   );
